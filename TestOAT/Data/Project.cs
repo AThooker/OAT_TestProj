251df014c523f36bd5066a917642a76da6df79f8
Austin Hooker <ahooker@elevenfifty.org>
2022-06-29 16:53:25 -0400

    testOAT app / deciding what views to have and layout

---
+++ TestOAT/Data/Project.cs
@@ -0,0 +1,10 @@
+﻿namespace TestOAT.Data
+{
+    public class Project
+    {
+        public int ID { get; set; }
+        public string? Description { get; set; }
+        public double PurchasePrice { get; set; }
+        public bool IsCompleted { get; set; }
+    }
+}
